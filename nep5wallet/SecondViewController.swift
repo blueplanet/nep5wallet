07943fbaf79c00ce3583f85c83806655d7af6587
blueplanet <gyo@re-work.io>
2018-02-21 05:19:21 +0900

    Initial Commit

---
+++ nep5wallet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  nep5wallet
+//
+//  Created by Shinkou Gyo on 2018/02/21.
+//  Copyright © 2018年 re-work.io. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
